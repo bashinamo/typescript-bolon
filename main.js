1cae0febb59080205fc8704cf73968c384583b46
bashin <goblinsgoldg@gmail.com>
2024-02-06 00:45:27 +0100

    klar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// ha koppling till ingångarna för lån, ränta och tid
-const loanInput = document.getElementById("lån");
-const interestInput = document.getElementById("ränta");
+// ha koppling till ingångarna för lon, ränta och tid
+const loanInput = document.getElementById("lon");
+const interestInput = document.getElementById("renta");
 const timeInput = document.getElementById("time");
 
 
@@ -13,57 +13,52 @@ function calculateM(P, r, n) {
 }
 
 // Lägger till händelse för knappen
-submitBtn.addEventListener("click", () => {});
+submitBtn.addEventListener("click", () => {
+
+});
 
 // värden måste konverteras från string till heltal/tal
-let lån = Number.lånInput.value
-console.log(lån);
+let lon = Number(loanInput.value)
 
-const ränta = Number.räntaInput.value / 100 / 12;
-console.log(ränta);
+const renta = Number(interestInput.value) / 100 / 12;
 
-const time = Number.timeInput.value * 12;
-console.log(time);
+const time = Number(timeInput.value) * 12;
 
-// Radera tidigare resultat/felmeddelanden
-deleteResults();
-    const errorMessage = document.createElement("p");
-    const errorMessage2 = document.createElement("p");
 
 // Kontrollera om tiden är längre än 50 /= år
 if (time / 12 > 50) {
-    errorMessage.innerHTML = `Återbetalningstiden är för lång!`;
+    errorMessage.innerHTML = `Återbetalningstiden är för long!`;
     document.body.appendChild(errorMessage);
 }
 // Kontrollera om räntan är över 30 %
-if (ränta * 100 * 12 > 30) {
+if (renta * 100 * 12 > 30) {
     errorMessage2.innerHTML = `Räntan du har angett är för hög!`;
     document.body.appendChild(errorMessage2);
 }
  // Beräkna månadskostnaden
- const månadskostnad = Number(calculateM(lån, ränta, time).toFixed(2));
+ const monadskostnad = Number(calculateM(lon, renta, time).toFixed(2));
 
  // Beräkna den totala ränteavgiften
- const totalRänta = lån * ränta * time;
+ const totalRenta = lon * renta * time;
     // Presentera resultat för kuunden om den gick igenom
-    if (!(ränta * 100 * 12 > 30) && !(time / 12 > 50)) {
+    if (!(renta * 100 * 12 > 30) && !(time / 12 > 50)) {
         const results = document.createElement("p");
         results.innerHTML = `
-            Din totala månadskostnad blir ${månadskostnad} kronor.<br>
-            Din totala räntekostnad över hela låneperioden blir ${totalRänta} kronor.`;
+            Din totala månadskostnad blir ${monadskostnad} kronor.<br>
+            Din totala räntekostnad över hela loneperioden blir ${totalRenta} kronor.`;
         document.body.appendChild(results);
     }
     // En betalningsplan
-    if (!(ränta * 100 * 12 > 30) && !(time / 12 > 50)) {
+    if (!(renta * 100 * 12 > 30) && !(time / 12 > 50)) {
         for (let month = 1; month <= time; month++) {
-            const ränteBetalning = lån * ränta;
-            const avbetalning = månadskostnad - ränteBetalning;
-            lån -= avbetalning;
-            if (lån <= 0) {
+            const renteBetalning = lon * renta;
+            const avbetalning = monadskostnad - renteBetalning;
+            lon -= avbetalning;
+            if (lon <= 0) {
                 break;
             }
             const results2 = document.createElement("p");
-            results2.innerHTML = `Månad ${month}:&nbsp;${lån.toFixed(2)}`;
+            results2.innerHTML = `Månad ${month}:&nbsp;${lon.toFixed(2)}`;
             document.body.appendChild(results2);
         }
     }
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
-const lånInput = document.getElementById("loan") as HTMLInputElement;
-const räntaInput = document.getElementById("interest") as HTMLInputElement;
+const lonInput = document.getElementById("lon") as HTMLInputElement;
+const rentaInput = document.getElementById("renta") as HTMLInputElement;
 const timeInput = document.getElementById("time") as HTMLInputElement;
 
 
@@ -11,16 +11,15 @@ function calculateM(P: number, r: number, n: number): number {
 }
 
 
+
 submitBtn.addEventListener("click", () => {
 
-    let lån: number = Number.lånInput.value;
-    console.log(lån);
-    const ränta: number = Number.räntaInput.value / 100 / 12;
-    console.log(ränta);
-    const time: number = Number.timeInput.value * 12;
+    let lon: number = Number(lonInput.value);
+    const renta: number = Number(rentaInput.value) / 100 / 12;
+    const time: number = Number(timeInput.value) * 12;
     console.log(time);
 
-  deleteResults();
+  
 
   const errorMessage = document.createElement("p");
   const errorMessage2 = document.createElement("p");
@@ -31,38 +30,38 @@ submitBtn.addEventListener("click", () => {
   } 
 
   
-  if (ränta * 100 * 12 > 30) {
+  if (renta * 100 * 12 > 30) {
     errorMessage2.innerHTML = `Räntan du har angett är för hög!`;
     document.body.appendChild(errorMessage2);
   } 
 
-  const månadskostnad: number = Number(
-    calculateM(lån, ränta, time).toFixed(2)
+  const monadskostnad: number = Number(
+    calculateM(lon, renta, time).toFixed(2)
   );
 
   
-  const totalRänta: number = lån * ränta * time;
+  const totalRenta: number = lon * renta * time;
 
   
-  if (!(ränta * 100 * 12 > 40) && !(time / 12 > 50)) {
+  if (!(renta * 100 * 12 > 40) && !(time / 12 > 50)) {
     const results = document.createElement("p");
-    results.innerHTML = `Din totala månadskostnad blir ${månadskostnad} kronor.
-    Din totala räntekostnad över hela låneperioden blir ${totalRänta} kronor.`;
+    results.innerHTML = `Din totala månadskostnad blir ${monadskostnad} kronor.
+    Din totala räntekostnad över hela låneperioden blir ${totalRenta} kronor.`;
     document.body.appendChild(results);
   }
 
   
-  if (!(ränta * 100 * 12 > 30) && !(time / 12 > 50)) {
+  if (!(renta * 100 * 12 > 30) && !(time / 12 > 50)) {
 
     for (let month = 1; month <= time; month++) {
-      const ränteBetalning: number = lån * ränta;
-      const avbetalning: number = månadskostnad - ränteBetalning;
-      lån -= avbetalning;
-      if (lån <= 0) {
+      const renteBetalning: number = lon * renta;
+      const avbetalning: number = monadskostnad - renteBetalning;
+      lon -= avbetalning;
+      if (lon <= 0) {
     
       }
-      const results2 = document.createElement("p");lån
-      results2.innerHTML = `Månad ${month}:&nbsp;${lån.toFixed(
+      const results2 = document.createElement("p");lon
+      results2.innerHTML = `Månad ${month}:&nbsp;${lon.toFixed(
         2
       )}`;
 
